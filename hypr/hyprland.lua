71e0cb5102a35ab10b5b74bceb29787d0de1c196
tobitdsm <tobitdesmet@gmail.com>
2026-08-13 15:10:48 +0200

    fix compatibility with new update + add compose key for characters like ø, æ and å

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -177,7 +177,7 @@ hl.curve("almostLinear",   { type = "bezier", points = { {0.5, 0.5},   {0.75, 1}
 hl.curve("quick",          { type = "bezier", points = { {0.15, 0},    {0.1, 1}     } })
 
 -- Default springs
-hl.curve("easy",           { type = "spring", mass = 1, stiffness = 71.2633, dampening = 15.8273644 })
+hl.curve("easy",           { type = "spring", mass = 1, stiffness = 213.7899, dampening = 23.7410466 })
 
 hl.animation({ leaf = "global",        enabled = true,  speed = 10,   bezier = "default" })
 hl.animation({ leaf = "border",        enabled = true,  speed = 5.39, bezier = "easeOutQuint" })
@@ -258,7 +258,7 @@ hl.config({
         kb_layout  = "be",
         kb_variant = "",
         kb_model   = "",
-        kb_options = "",
+        kb_options = "compose:rctrl",
         kb_rules   = "",
 
         follow_mouse = 0,
